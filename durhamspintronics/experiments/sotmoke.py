3c8e447f25a0be9ca42f3aaeef6e2150ed0c350c
Ben Nicholson <ben_nicholson@live.co.uk>
2024-03-06 16:52:13 +0000

    Update sotmoke.py

--- durhamspintronics/experiments/sotmoke.py
+++ durhamspintronics/experiments/sotmoke.py
@@ -1,6 +1,6 @@
 from durhamspintronics.instruments.ni import pci6034e
 
 def get_frequency():
-  input_device = pci6034e
+  input_device = pci6034e()
   print(f'Frequency = {input_device.frequency}')
   return input_device.frequency

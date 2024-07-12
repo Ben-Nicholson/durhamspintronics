9c12e036fb1c9c833c8b9102ba7a9fd27493f27d
Ben Nicholson <ben_nicholson@live.co.uk>
2024-07-12 14:16:41 +0100

    Update __init__.py

--- durhamspintronics/__init__.py
+++ durhamspintronics/__init__.py
@@ -1,5 +1,5 @@
 # This program is in the public domain
-# Author: ben Nicholson
+# Author: Ben Nicholson
 """
 A collection of instrument control and analysis tools for the Spintronics group at Durham University.
 """
